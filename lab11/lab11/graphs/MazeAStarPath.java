10d3adb277377689ec893c21b3a788ab67e68891
Palm <pmmxiaoxiaomayi@gmail.com>
2023-01-19 03:21:27 +0800

    cycle and Astar

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,10 @@
 package lab11.graphs;
 
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -8,7 +13,7 @@ public class MazeAStarPath extends MazeExplorer {
     private int t;
     private boolean targetFound = false;
     private Maze maze;
-
+    private LinkedList<Integer> unvisited = new LinkedList<>();
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         maze = m;
@@ -16,24 +21,68 @@ public class MazeAStarPath extends MazeExplorer {
         t = maze.xyTo1D(targetX, targetY);
         distTo[s] = 0;
         edgeTo[s] = s;
+        for (int v = 0; v < maze.V(); v++) {
+            unvisited.add(v);
+        }
     }
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        int vX = maze.toX(v);
+        int vY = maze.toY(v);
+        int tX = maze.toX(t);
+        int tY = maze.toY(t);
+        return Math.abs(vY - tY) + Math.abs(vX - tX);
     }
 
     /** Finds vertex estimated to be closest to target. */
     private int findMinimumUnmarked() {
-        return -1;
+        cmparatorHeur cmpt = new cmparatorHeur();
+        Queue<Integer> fringe = new PriorityQueue<>(cmpt);
+        for (int v : unvisited) {
+            fringe.add(v);
+        }
+
+        return fringe.poll();
         /* You do not have to use this method. */
     }
 
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
-        // TODO
+        marked[s] = true;
+        while (!unvisited.isEmpty()) {
+            int v = findMinimumUnmarked();
+            marked[v] = true;
+            announce();
+            unvisited.remove((Object) v);
+            if (v == t) {
+                targetFound = true;
+                return;
+            }
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    distTo[w] = distTo[v] + 1;
+                    edgeTo[w] = v;
+                    if (w == t) {
+                        marked[w] = true;
+                        targetFound = true;
+                        announce();
+                        return;
+                    }
+                }
+            }
+        }
+
+
     }
 
+    private class cmparatorHeur implements Comparator<Integer> {
+
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return h(o1) + distTo[o1] - h(o2) - distTo[o2];
+        }
+    }
     @Override
     public void solve() {
         astar(s);
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Random;
+
 /**
  *  @author Josh Hug
  */
@@ -9,16 +13,51 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
-
+    private int s;
+    private int[] parent;
+    private boolean hasCycle = false;
     public MazeCycles(Maze m) {
         super(m);
+        s = 0;
+        parent = new int[maze.V()];
+        parent[0] = 0;
+
+
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfsCycle(s);
+
     }
 
     // Helper methods go here
+    private void dfsCycle(int v) {
+        marked[v] = true;
+        if (hasCycle) {
+            return;
+        }
+        for (int w : maze.adj(v)) {
+            if (!marked[w]) {
+                parent[w] = v;
+                announce();
+                dfsCycle(w);
+            } else if (marked[w] && parent[v] != w) {
+                connectCycle(w, v);
+                announce();
+                hasCycle = true;
+                return;
+            }
+        }
+    }
+
+    private void connectCycle(int w, int v) {
+        edgeTo[w] = v;
+        int c = v;
+        while (edgeTo[v] != c) {
+            edgeTo[v] = parent[v];
+            v = parent[v];
+        }
+    }
 }
 
